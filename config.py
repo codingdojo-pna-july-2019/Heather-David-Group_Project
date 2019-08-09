67103d6566b05da5d57742e896d01d8853573430
Timeless <davidrk88@gmail.com>
2019-08-08 18:16:48 -0700

    removed Twitter API creds

--- config.py
+++ config.py
@@ -15,10 +15,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Twitter API - Variables for user credentials
-ACCESS_TOKEN = '2895347772-W3Zt38VsyKxbo89qhZQ4py1ZVUVojghEA66e3b8'
-ACCESS_SECRET = '4Rb1n3kl5pduXoc3tdQefA6QfKsqrxLa3av522CAvB3hx'
-CONSUMER_KEY = 'yrTkyVCop7SXd8RL2qeALS5sR'
-CONSUMER_SECRET = 'hiifkqG1a0Pi87Jww4dyD0VBokREinn0xtQM0pkmSpOAFJDQRr'
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 # Setup tweepy to authenticate with Twitter credentials:
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
